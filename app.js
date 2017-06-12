6a8dd72c7c64bd6ff32074c19fa508679e99583e
Rick Aguayo <rick.aguayo@gmail.com>
2017-06-12 17:33:11 -0500

    added nunjucks

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+
+// app.use(function(req, res, next) {
+//   res.status(200);
+//   console.log(req.method + ' ' + req.url + ' ' + res.statusCode);
+//   next();
+// })
+
+app.use(morgan('dev'));
 
 app.get('/', function(req,res, next){
-  console.log('in get');
   res.send('Welcome!');
+  next();
+})
+
+app.get('/news', function(req,res, next){
+  console.log('in get');
+  res.send('Welcome to the news page!');
 })
 
 app.listen(3000, function() {
